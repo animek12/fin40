2f51c4d50c290a5ec1b6af34ac4a61321adfb881
animek12 <111511065+animek12@users.noreply.github.com>
2022-10-31 18:49:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
+global.owner = ['6282261065144']
+global.ownernomer = "6282261065144"
+global.premium = ['6282261065144']
 global.packname = 'Sticker By'
-global.author = 'Naze Dev'
+global.author = 'Fin Bot'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
@@ -59,15 +59,15 @@ global.thumb = fs.readFileSync('./media/image/naze.jpg')
 global.faall = fs.readFileSync('./media/image/fake.jpg')
 
 // Url
-global.mygit = 'https://github.com/nazedev'
-global.myyt = 'https://youtube.com/c/Nazedev'
-global.myytv = 'https://youtu.be/FAsL-Jy4qLc'
+global.mygit = 'https://github.com/Raya2399'
+global.myyt = 'https://linktr.ee/fin222'
+global.myytv = 'https://tiktok.com/alvin_ch1'
 global.mygc = "https://chat.whatsapp.com/Cp1OJenk6Q9D9vgLjLU558"
 
 // Engak Ngaruh, Belum Kepasang
-global.botname = 'NAZE BOT'
-global.akulaku = 'Bot By Naze'
-global.ytname = 'YT NAZE' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+global.botname = 'Fin Bot'
+global.akulaku = 'Bot By Fin'
+global.ytname = 'YT ALWANGANZZ' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
 
 
 let file = require.resolve(__filename)
